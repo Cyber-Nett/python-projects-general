a11a630d054da20c809255de195f9cedb06696e4
Cyber-Nett <hinhab1@gmail.com>
2023-11-18 15:27:08 +0200

    literal **

--- while/3.py
+++ while/3.py
@@ -2,11 +2,9 @@ a=int(input())
 num=0
 num=a
 counter=0
-while a!=0:
+while a>0:
     divnum = a//10
     print(divnum)
     a=(a-divnum)/10
     counter+=1
 print(counter)
-if counter//2==0:
-    print('парне')
